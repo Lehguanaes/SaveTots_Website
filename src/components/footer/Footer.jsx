728db5a47c53a1a06844ff1c88d5d9dda85dd3fb
Kakventura <kakaangelo25@gmail.com>
2025-11-27 16:27:58 -0300

    Subindo Alterações do Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,52 @@
 import './Footer.css';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo/logo-footer.png';
 
 export default function Footer() {
-  return (
-    <footer className="footer-container">
-      <p>© {new Date().getFullYear()} SaveTots — Todos os direitos reservados.</p>
-    </footer>
-  );
+
+    function handleEmailClick(e) {
+        e.preventDefault();
+        window.location.href = "mailto:savetotsinfo@gmail.com";
+    }
+
+    return (
+        <footer className="footer">
+            <div className="container">
+
+                {/* COLUNA LOGO */}
+                <div className="coluna logoColuna">
+                    <img src={logo} className="logo" alt="Logo SaveTots" />
+                </div>
+
+                {/* COLUNA LINKS */}
+                <div className="coluna linksColuna">
+                    <h4>Navegação</h4>
+                    <ul>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/sobre">Sobre o SaveTots</Link></li>
+                    </ul>
+                </div>
+
+                {/* COLUNA CONTATO */}
+                <div className="coluna contatoColuna">
+                    <h4>Contate-nos</h4>
+                    <p>
+                        <a 
+                            href="#"
+                            onClick={handleEmailClick}
+                            className="emailLink"
+                        >
+                            savetotsinfo@gmail.com
+                        </a>
+                    </p>
+                </div>
+
+            </div>
+
+            <p className="direitos">© 2025 SaveTots. Todos os direitos reservados. </p>
+
+              
+
+        </footer>
+    );
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,7 @@
 import Carrossel from '../../components/carrossel/Carrossel.jsx';
 import Gatilhos from '../../components/sections/Gatilhos.jsx';
 
-// IMPORTANDO SUAS IMAGENS
-import fotoPet from '../../assets/home/fotopet.png';
+import fotoPet from '../../assets/home/fotopet.jpg';
 import fotoChaveiro from '../../assets/home/fotoChaveiro.png';
 import fotoCuidadora from '../../assets/home/fotoCuidadora.png';
 
@@ -14,41 +13,31 @@ export default function Home() {
       <Carrossel />
       <Gatilhos />
 
-      {/* SEÇÃO DAS FOTOS COM TEXTO */}
-      <div className={styles.cardsWrapper}>
+      {/* IMAGENS SEPARADAS COM CLASSES INDIVIDUAIS */}
+      <section className={styles.gallery}>
 
-        {/* CARD GRANDE DO CHAVEIRO */}
-        <div className={styles.cardGrande}>
-          <img src={fotoChaveiro} alt="Chaveiro SaveTots" className={styles.imgGrande}/>
+        <div className={`${styles.item} ${styles.itemChaveiro}`}>
+          <img src={fotoChaveiro} alt="Chaveiro SaveTots" className={styles.imgChaveiro}/>
           <p>
-            O SaveTots foi projetado para ser o seu maior aliado na segurança da sua criança,
-            trazendo praticidade, leveza e um alerta rápido em situações de risco.
+            O SaveTots foi projetado para ser pratico e eficiente.
           </p>
         </div>
 
-        {/* LINHA COM DOIS CARDS (PET E CUIDADORA) */}
-        <div className={styles.cardLinhaBaixo}>
-
-          {/* CARD DO PET */}
-          <div className={styles.card}>
-            <img src={fotoPet} alt="Pet seguro no transporte" />
-            <p>
-              Com o SaveTots, seu animalzinho fica seguro durante todo o transporte veicular, 
-              garantindo conforto e proteção para os seus melhores amigos.
-            </p>
-          </div>
-
-          {/* CARD DA CUIDADORA */}
-          <div className={styles.card}>
-            <img src={fotoCuidadora} alt="Cuidadora com SaveTots" />
-            <p>
-              Pensado também para cuidadores: o SaveTots facilita o dia a dia de quem é responsável
-              pelo bem-estar das crianças, oferecendo mais segurança e tranquilidade.
-            </p>
-          </div>
-
+        <div className={`${styles.item} ${styles.itemPet}`}>
+          <img src={fotoPet} alt="Pet seguro no transporte" className={styles.imgPet}/>
+          <p>
+          Além da segurança, o SaveTots traz conforto e proteção para os seus melhores amigos.
+          </p>
         </div>
-      </div>
+
+        <div className={`${styles.item} ${styles.itemCuidadora}`}>
+          <img src={fotoCuidadora} alt="Cuidadora com SaveTots" className={styles.imgCuidadora}/>
+          <p>
+            Pensado também para cuidadores: o dispositivo facilita o dia a dia de quem é responsável.
+          </p>
+        </div>
+
+      </section>
     </div>
   );
 }
